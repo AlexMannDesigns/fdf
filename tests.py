fc2b9ec5672fd8356bfdbfc3d1d209b9b782c4b7
Alex Mann <alex.mann@wolt.com>
2023-11-24 11:12:54 +0200

    move test map into subdir, start pytest setup for testing map parser

---
+++ tests.py
@@ -0,0 +1,34 @@
+from subprocess import run
+from typing import Final
+
+import pytest
+
+
+FDF: Final = "fdf"
+TEST_FLAG: Final = "--test-parser"
+TIMEOUT: Final = 5
+
+
+@pytest.mark.parametrize(
+	("path_to_map", "expected_error_output", "expected_return_value"),
+	[
+		("maps/42.fdf", None, 0),
+	],
+)
+def test_maps(
+	path_to_map: str,
+	expected_error_output: str | None,
+	expected_return_value: int) -> None:
+	try:
+		result = run(
+			[FDF, TEST_FLAG, path_to_map],
+			timeout=TIMEOUT,
+			capture_output=True,
+			test=True,	
+		)
+	except TimeoutExpired:
+		print(f"map: {path_to_map}: timeout expired, please review!")
+	
+	assert result.returncode == expected_return_value
+	assert result.stderr == expected_error_output
+
